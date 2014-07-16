95da2077a604db216eb1da91fb6cd7568be9345e
Daniel Stern <daniel@danielstern.ca>
2014-07-16 12:52:48 -0400

    fixed with app dir

--- gruntfile.js
+++ gruntfile.js
@@ -1,42 +1,49 @@
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.initConfig({
-		// Turns LESS files into CSS files
-		less: {
-		    dev: {
-		        options: {
-		            paths: [""],
-		            compress:true,
-		            sourceMap: true,
-		            sourceMapFilename: 'css/compiled.css.map',
-		            sourceMapRootpath: '../',
-		        },
-		        files: {
-		            "css/compiled.css": "less/styles.less",
-		        }
-		    },
-		},
-		connect: {
-		    options: {
-		        port: 666,
-		        open: true,
-		        hostname: 'localhost'
-		    },
-		    livereload: {
-		       
-		    },
-		},
-		watch: {
-		   
-		},
-	});
+    grunt.initConfig({
+        // Turns LESS files into CSS files
+        less: {
+            dev: {
+                options: {
+                    paths: [""],
+                    compress: true,
+                    sourceMap: true,
+                    sourceMapFilename: 'app/css/compiled.css.map',
+                    sourceMapRootpath: '../',
+                    sourceMapBasepath: 'app/',
+                },
+                files: {
+                    "app/css/compiled.css": "app/less/styles.less",
+                }
+            },
+        },
+        connect: {
+            options: {
+                port: 666,
+                open: true,
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    middleware: function(connect) {
+                        return [
+                            connect.static("app/")
+                        ];
+                    },
+                }
+            },
+        },
+        watch: {
 
-	grunt.registerTask('default', [
-	    'less',
-	    'connect:livereload',
-	    'watch'
-	]);
+        },
+    });
+
+    grunt.registerTask('default', [
+        'less',
+        'connect:livereload',
+        'watch'
+    ]);
 }
